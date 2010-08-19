b03d703ca5a0c0bdfff44d8b0031790af698c22f
Grant Bowman <grantbow@gmail.com>
2010-08-19 06:01:42 -0700

    First splash.java checkin - needs work.

---
+++ src/com/activehabits/android/splash.java
@@ -0,0 +1,30 @@
+package com.activehabits.android;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class splash extends Activity {
+    //private static final String TAG = "ActiveHabits.mark";
+	//private static OnClickListener mSplashListener;
+    
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //Log.i(TAG, "splash");
+        setContentView(R.layout.splash);
+    }
+    
+//    private OnClickListener mSplashListener = new OnClickListener() {
+//        public void onClick(View v) {
+//          // finish when the button is clicked
+//            finish();
+//        }
+//    };
+
+//    public void onClick(View v) {
+//    	finish();
+//    }
+
+//    public void onTouch(View v) {
+//    	finish();
+//    }
+}
